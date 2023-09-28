48c3b5f37acd6784a24524c16e565430965826c0
T4der <85561351+T4der@users.noreply.github.com>
2023-09-28 10:36:14 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,5 +4,5 @@ rad = float(input("Введіть радіус кола: "))
 dov = 2 * math.pi * rad
 area = math.pi * (rad ** 2)
 
-print(f"Довжина кола: {dov}")
-print(f"Площа круга: {area}")
+print("Довжина кола: " dov)
+print("Площа круга: " area)
